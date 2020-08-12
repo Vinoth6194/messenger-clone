a842654fb813bd9215c9ab26073be4f6e8c71373
vin-unicity <vinoth.kumar@unicity.com>
2020-08-12 17:23:28 +0530

    msgs displayed using map fun

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages,setMessages] = useState(["Hi Bro","Vanakam", "Mahalo"]);
+  const [messages, setMessages] = useState(["Hi Bro", "Vanakam", "Mahalo"]);
   console.log(input);
   console.log(messages);
 
-  const sendMessage = (event)  =>{
-    setMessages([...messages,input]);
-    setInput('');
-  }
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-    <h1>Hello Clever Programmers :🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 </h1>
-    <input value ={input} onChange ={event =>setInput(event.target.value)}></input>
-    <button onClick ={sendMessage}>Send Message</button>
-    {/* Input Field */}
-    {/* Button */}
-    {/* Messages */}
+      <h1>Hello Clever Programmers :🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 </h1>
+      {/* Input Field */}
+
+      <input
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      ></input>
+      {/* Button */}
+
+      <button onClick={sendMessage}>Send Message</button>
+      {/* Messages */}
+
+    {messages.map(message =>(
+      <p>{message}</p>
+    ))}
     </div>
   );
 }
