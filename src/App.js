2ca43aec6b070887bb934bf4272d394f588d762f
vin-unicity <vinoth.kumar@unicity.com>
2020-08-12 17:14:28 +0530

    messages are added to the array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages,setMessages] = useState(["Hi Bro","Vanakam", "Mahalo"]);
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event)  =>{
+    setMessages([...messages,input]);
+    setInput('');
+  }
   return (
     <div className="App">
     <h1>Hello Clever Programmers :🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 </h1>
+    <input value ={input} onChange ={event =>setInput(event.target.value)}></input>
+    <button onClick ={sendMessage}>Send Message</button>
+    {/* Input Field */}
+    {/* Button */}
+    {/* Messages */}
     </div>
   );
 }
