197d6a20a7e96e64ab3b632bd45f690c7f1d0b05
vin-unicity <vinoth.kumar@unicity.com>
2020-08-15 03:01:49 +0530

    unknown user name fixed

--- src/Message.js
+++ src/Message.js
@@ -9,7 +9,7 @@ const Message =forwardRef (({message,username}, ref) => {
             <Card >
                 <CardContent className ={isUser ? "message__userCard" : "message__guestCard"}>
                     <Typography color = "white" variant = "h5" component = "h2">
-    {!isUser && `${message.username}: `}{message.message}
+    {!isUser && `${message.username || 'Unknown User'} : `}{message.message}
                     </Typography>
                 </CardContent>
             </Card>
