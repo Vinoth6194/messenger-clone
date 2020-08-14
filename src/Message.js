10fed3fba346196aa5e9dcefcd6910297a8e1750
vin-unicity <vinoth.kumar@unicity.com>
2020-08-15 02:13:26 +0530

    Sender name removed in msg disp

--- src/Message.js
+++ src/Message.js
@@ -9,7 +9,7 @@ const Message =forwardRef (({message,username}, ref) => {
             <Card >
                 <CardContent className ={isUser ? "message__userCard" : "message__guestCard"}>
                     <Typography color = "white" variant = "h5" component = "h2">
-                {message.username}:{message.message}
+    {!isUser && `${message.username}: `}{message.message}
                     </Typography>
                 </CardContent>
             </Card>
